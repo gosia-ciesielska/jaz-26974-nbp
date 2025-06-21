923e0e863faebd38c12c9e3c136a1819a24283cb
Malgorzata Ciesielska <s26974@pjwstk.edu.pl>
2025-06-21 15:09:58 +0200

    initialized repo

---
+++ src/main/java/pl/pjatk/jaz_26974_nbp/Jaz26974NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_26974_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz26974NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaz26974NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jaz_26974_nbp/Jaz26974NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_26974_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz26974NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
